4143a224fb6a536300a3533a2e2cfa2fbf5df5ea
unknown <jacqueline2024csuiza@gmail.com>
2025-03-17 17:04:35 -0300

    mi pirmer commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+var valores = [true, 5, false, "hola", "adios", 2]; 
+function eje(){ 
+    if (valores[4].length > valores[3].length){
+        console.log("El valor "+valores[4]+ " es mayor que "+valores[3]);  
+        console.log(valores[0]);
+    }
+    else {
+        console.log("El valor "+valores[3]+ " es menor que "+valores[4]);  
+        console.log(valores[2]); 
+    }
+}
+    console.log(5 + 2); 
+    console.log(5 - 2); 
+    console.log(5 * 2); 
+    console.log(5 / 2); 
+    console.log(5 % 2); 
